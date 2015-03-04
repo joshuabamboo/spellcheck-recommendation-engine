0d83e676e000f63c8cb242a9edea007b08638dfc
joshuabamboo <joshuaowens011@gmail.com>
2015-03-03 23:28:28 -0500

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150302001731) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "levenshtein_distances", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
